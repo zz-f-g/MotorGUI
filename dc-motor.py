958957a2464215969b9386aa476d1efa562aa222
zz-f-g <zzguo2052110@outlook.com>
2022-11-29 22:13:55 +0800

    More features
    - adjust layout
    - add sample, plot, lstsq
    - scale unit for input

--- dc-motor.py
+++ dc-motor.py
@@ -1,5 +1,11 @@
-import tkinter as tk
 import sys
+import tkinter as tk
+from tkinter import messagebox
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from math import pi
 
 
@@ -11,6 +17,7 @@ class DcMotor:
         self.Rf = 181.5
         self.U = 220
         self.T2 = 0
+        self.Tm = 3
 
     @property
     def Omega(self) -> float:
@@ -31,54 +38,167 @@ class Ui:
     def __init__(self) -> None:
         self.M = DcMotor()
         self.M.T2 = 17e3/(3000 * 2 * pi / 60)
+        self.T2max = 95
+
+        self.data = {}
+        self.data['n'] = np.array([])
+        self.data['Ia'] = np.array([])
+
         self.root = tk.Tk()
         self.root.title('直流电动机工作特性测定')
         self.root.iconbitmap(sys.path[0] + '/images/motor.ico')
-        self.canvas = tk.Canvas(
+
+        self.canvas_sch = tk.Canvas(
             self.root,
-            width=600,
+            width=500,
             height=400,
-            bg='#ffffff')
+            bg='#ffffff'
+        )
         self.schematic = tk.PhotoImage(
             file=sys.path[0]+'/images/schematic.png')
-        self.canvas.create_image(300, 200, image=self.schematic)
-        self.canvas.grid(rowspan=8)
+        self.canvas_sch.create_image(250, 200, image=self.schematic)
+        self.canvas_sch.grid(columnspan=4)
+
         self.msg_T2 = tk.Message(
             self.root,
             text='输入电机负载（单位 N·m）',
-            width=150)
-        self.msg_T2.grid(row=0, column=1, columnspan=2)
-        self.entry_T2 = tk.Entry(self.root)
-        self.entry_T2.grid(row=1, column=1)
+            width=150
+        )
+        self.msg_T2.grid(row=1, column=0, columnspan=2)
+
+        self.T2str = tk.StringVar()
+        self.T2str.set('%.2f' % self.M.T2)
+        self.entry_T2 = tk.Entry(self.root, textvariable=self.T2str)
+        self.entry_T2.grid(row=2, column=0)
+
         self.btn_T2 = tk.Button(
             self.root,
             text='确认',
-            command=self.update)
-        self.btn_T2.grid(row=1, column=2)
-        self.n = tk.StringVar()
-        self.n.set('电机转速为\n%.0f r/min' % (self.M.n))
-        self.Ia = tk.StringVar()
-        self.Ia.set('电枢电流为\n%.2f A' % (self.M.Ia))
+            command=self.update
+        )
+        self.btn_T2.grid(row=2, column=1)
+
+        self.nstr = tk.StringVar()
+        self.nstr.set('电机转速为\n%.0f r/min' % (self.M.n))
         self.msg_n = tk.Message(
             self.root,
-            textvariable=self.n,
-            width=150)
-        self.msg_n.grid(row=4, column=1, rowspan=2, columnspan=2)
+            textvariable=self.nstr,
+            width=150
+        )
+        self.msg_n.grid(row=1, column=2, rowspan=2)
+
+        self.Iastr = tk.StringVar()
+        self.Iastr.set('电枢电流为\n%.2f A' % (self.M.Ia))
         self.msg_Ia = tk.Message(
             self.root,
-            textvariable=self.Ia,
-            width=150)
-        self.msg_Ia.grid(row=7, column=1, rowspan=2, columnspan=2)
+            textvariable=self.Iastr,
+            width=150
+        )
+        self.msg_Ia.grid(row=1, column=3, rowspan=2)
+
+        self.msg_R = tk.Message(
+            self.root,
+            text='或者调节右侧负载电阻 R',
+            width=150
+        )
+        self.msg_R.grid(row=3, column=0, columnspan=2)
+
+        self.scale_R = tk.Scale(
+            self.root,
+            label='拖动滑动条改变电阻 R：',
+            from_=30,
+            to=100,
+            orient=tk.HORIZONTAL,
+            length=200,
+            showvalue=False,
+            command=self.getR
+        )
+        self.scale_R.set(self.T2max / self.M.T2)
+        self.scale_R.grid(row=4, column=0, columnspan=2)
+
+        self.btn_sample = tk.Button(
+            self.root,
+            text='采样',
+            command=self.sample
+        )
+        self.btn_sample.grid(row=3, column=2, pady=5)
+
+        self.btn_plot = tk.Button(
+            self.root,
+            text='绘图',
+            command=self.plot
+        )
+        self.btn_plot.grid(row=3, column=3, pady=5)
+
+        self.btn_fit = tk.Button(
+            self.root,
+            text='拟合',
+            command=self.fit
+        )
+        self.btn_fit.grid(row=4, column=2, pady=5)
+
+        self.btn_clear = tk.Button(
+            self.root,
+            text='清除',
+            command=self.clear
+        )
+        self.btn_clear.grid(row=4, column=3, pady=5)
+
+        self.plot()
         self.root.mainloop()
 
     def update(self) -> None:
         try:
             T2 = float(self.entry_T2.get())
+            if (T2 < 0):
+                messagebox.askretrycancel('错误', '输入的负载转矩小于零！')
+                return
+            elif (T2 > self.T2max):
+                messagebox.askretrycancel('错误', '输入的负载转矩超范围！')
+                return
             self.M.T2 = T2
-            self.n.set('电机转速为\n%.0f r/min' % (self.M.n))
-            self.Ia.set('电枢电流为\n%.2f A' % (self.M.Ia))
+            self.scale_R.set(30 * self.T2max / T2)
+            self.nstr.set('电机转速为\n%.0f r/min' % (self.M.n))
+            self.Iastr.set('电枢电流为\n%.2f A' % (self.M.Ia))
         except:
-            print('> [!error] invalid input T2')
+            messagebox.askretrycancel('错误', '输入的负载转矩不合法！')
+    
+    def getR(self, value) -> None:
+        self.M.T2 = 30 * self.T2max / float(value)
+        self.T2str.set('%.2f' % self.M.T2)
+        self.nstr.set('电机转速为\n%.0f r/min' % (self.M.n))
+        self.Iastr.set('电枢电流为\n%.2f A' % (self.M.Ia))
+
+    def sample(self) -> None:
+        self.data['n'] = np.append(self.data['n'], self.M.n)
+        self.data['Ia'] = np.append(self.data['Ia'], self.M.Ia)
+    
+    def plot(self) -> None:
+        self.figure = Figure((6, 6))
+        self.draw = self.figure.add_subplot(1, 1, 1)
+        self.draw.scatter(self.data['Ia'], self.data['n'])
+        self.draw.set_xlim((0, 150))
+        self.draw.set_ylim((2500, 3300))
+        self.draw.set_xlabel('Ia/A')
+        self.draw.set_ylabel('n/(r/min)')
+        self.canvas_plot = FigureCanvasTkAgg(self.figure, self.root)
+        self.canvas_plot.get_tk_widget().grid(row=0, column=4, rowspan=5)
+
+    def fit(self) -> None:
+        A = np.vstack(
+            [self.data['Ia'], np.ones(len(self.data['Ia']))]
+        ).T
+        a, b = np.linalg.lstsq(A, self.data['n'], rcond=None)[0] # n = a * Ia + b
+        x = np.linspace(0, 150, 151)
+        self.draw.plot(x, x * a + b)
+        self.draw.set_title('n = %.2f * Ia + %.2f' %(a, b))
+        self.canvas_plot = FigureCanvasTkAgg(self.figure, self.root)
+        self.canvas_plot.get_tk_widget().grid(row=0, column=4, rowspan=5)
+    
+    def clear(self) -> None:
+        self.data['n'] = np.array([])
+        self.data['Ia'] = np.array([])
+        self.plot()
 
 
 if __name__ == '__main__':
