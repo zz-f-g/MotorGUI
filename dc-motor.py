585d801f6b37b933c0350f4d99dd890bdfebad5a
zz-f-g <zzguo2052110@outlook.com>
2022-11-29 10:45:16 +0800

    basic UI app with input T2 and output n & Ia

--- dc-motor.py
+++ dc-motor.py
@@ -3,7 +3,7 @@ import sys
 from math import pi
 
 
-class Dc_motor:
+class DcMotor:
     def __init__(self):
         self.CT = 0.5514812838705541
         self.T0 = 4.5033484079373
@@ -27,25 +27,59 @@ class Dc_motor:
         return (self.U - self.CT * self.U / self.Rf * self.Omega) / self.Ra
 
 
-def main() -> None:
-    M = Dc_motor()
-    M.T2 = 17e3/(3000 * 2 * pi / 60)
-    root = tk.Tk()
-    root.title('直流电动机工作特性测定')
-    root.iconbitmap(sys.path[0] + '/images/motor.ico')
-    canvas = tk.Canvas(root, width=400, height=400, bg='#ffffff')
-    canvas.grid(rowspan=8)
-    canvas.create_rectangle(100, 100, 300, 300, width=3)
-    msg_T2 = tk.Message(root, text='输入电机负载（单位 N·m）', width=150)
-    msg_T2.grid(row=0, column=1)
-    entry_T2 = tk.Entry(root)
-    entry_T2.grid(row=1, column=1)
-    msg_n = tk.Message(root, text='电机转速为\n%.0fr/min' % (M.n), width=150)
-    msg_n.grid(row=4, column=1, rowspan=2)
-    msg_Ia = tk.Message(root, text='电枢电流为\n%.2fA' % (M.Ia), width=150)
-    msg_Ia.grid(row=7, column=1, rowspan=2)
-    root.mainloop()
+class Ui:
+    def __init__(self) -> None:
+        self.M = DcMotor()
+        self.M.T2 = 17e3/(3000 * 2 * pi / 60)
+        self.root = tk.Tk()
+        self.root.title('直流电动机工作特性测定')
+        self.root.iconbitmap(sys.path[0] + '/images/motor.ico')
+        self.canvas = tk.Canvas(
+            self.root,
+            width=600,
+            height=400,
+            bg='#ffffff')
+        self.schematic = tk.PhotoImage(
+            file=sys.path[0]+'/images/schematic.png')
+        self.canvas.create_image(300, 200, image=self.schematic)
+        self.canvas.grid(rowspan=8)
+        self.msg_T2 = tk.Message(
+            self.root,
+            text='输入电机负载（单位 N·m）',
+            width=150)
+        self.msg_T2.grid(row=0, column=1, columnspan=2)
+        self.entry_T2 = tk.Entry(self.root)
+        self.entry_T2.grid(row=1, column=1)
+        self.btn_T2 = tk.Button(
+            self.root,
+            text='确认',
+            command=self.update)
+        self.btn_T2.grid(row=1, column=2)
+        self.n = tk.StringVar()
+        self.n.set('电机转速为\n%.0f r/min' % (self.M.n))
+        self.Ia = tk.StringVar()
+        self.Ia.set('电枢电流为\n%.2f A' % (self.M.Ia))
+        self.msg_n = tk.Message(
+            self.root,
+            textvariable=self.n,
+            width=150)
+        self.msg_n.grid(row=4, column=1, rowspan=2, columnspan=2)
+        self.msg_Ia = tk.Message(
+            self.root,
+            textvariable=self.Ia,
+            width=150)
+        self.msg_Ia.grid(row=7, column=1, rowspan=2, columnspan=2)
+        self.root.mainloop()
+
+    def update(self) -> None:
+        try:
+            T2 = float(self.entry_T2.get())
+            self.M.T2 = T2
+            self.n.set('电机转速为\n%.0f r/min' % (self.M.n))
+            self.Ia.set('电枢电流为\n%.2f A' % (self.M.Ia))
+        except:
+            print('> [!error] invalid input T2')
 
 
 if __name__ == '__main__':
-    main()
+    app = Ui()
